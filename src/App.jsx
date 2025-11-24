ea4aadce9b71998a182399b428938f86ec855a65
Miguel Angel Zambrano <angel245687@gmail.com>
2025-11-23 21:50:07 -0300

    agrego maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import { Container } from "react-bootstrap"
-import logo from './assets/logoSimpsons.jpg'
+import { Container, Button, Spinner } from "react-bootstrap"
+import logo from './assets/logoSimpsons.png'
+import Frase from "./components/Frase"
+
+
 
 function App() {
 
@@ -7,8 +10,15 @@ function App() {
 
   return (
     <>
-    <Container className="text-center my-5" >
-    <img src={logo} alt="logo Simpsons" className="" />
+    <Container className="text-center my-5 col-12" >
+    <img src={logo} alt="logo Simpsons" className="w-50" />
+    <div>
+    <Spinner animation="grow" variant="ligth" />
+    </div>
+      <Frase></Frase>
+      <Button variant="warning" className="mt-4">Enviar</Button>
+    
+    
     </Container>
     </>
   )
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,10 +1,20 @@
+import Card from 'react-bootstrap/Card';
 
 
 
 const Frase = () => {
   return (
     <div>
-      
+        {/* <img src="" alt="" /> */}
+      <Card className='text-center' >
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
     </div>
   )
 }
